56789d9f0746fb5d4caabc36ee2c22a74365dd2d
gskayan <gskayan@Glebs-MacBook-Air.local>
2015-12-20 09:50:08 -0500

    added test for no throw

--- src/main_unit_test.cpp
+++ src/main_unit_test.cpp
@@ -2,12 +2,15 @@
 
 using namespace std;
 
-
 TEST (Practice, TestGtest)
 {
 	bool success(true);
 	ASSERT_TRUE(success);
 };
+TEST(Practice, PrintMessage_DoesNotThrow)
+{
+	EXPECT_NO_THROW(my_test_utils::printMessage("printMessage without Exception"));
+}
 
 int main(int argc, char* argv[])
 {
