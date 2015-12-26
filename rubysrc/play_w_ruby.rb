fae49c8dc36b60106eba6b7e33e6ca34770af64b
gskayan <gskayan@gmail.com>
2015-12-26 11:59:00 -0500

    testing code

--- rubysrc/play_w_ruby.rb
+++ rubysrc/play_w_ruby.rb
@@ -1,3 +1,24 @@
 !#/usr/bin/ruby
 
 1.upto(10) {puts "Learn Ruby"}
+
+def checkNum(i)
+  begin
+    if i.odd? then 
+      puts i**2
+    elsif i.even? then
+      puts Float(i/3)
+    end
+  end
+end
+
+for i in 1...9 do
+  checkNum(i)
+end
+$x=0
+
+until $x > 10 do
+  puts $x
+  checkNum($x);
+  $x +=1;
+end
