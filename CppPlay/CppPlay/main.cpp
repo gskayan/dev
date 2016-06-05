bb4738e66bae9ee5069eeda5c8c17ddff204a824
gskayan <gskayan@gmail.com>
2016-06-05 14:40:57 -0400

    .

---
+++ CppPlay/CppPlay/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CppPlay
+//
+//  Created by Gleb Skayansky on 4/10/16.
+//  Copyright © 2016 Gleb Skayansky. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ include/field_pool.h
@@ -0,0 +1,13 @@
+//
+//  field_pool.h
+//  CppPractice
+//
+//  Created by Gleb Skayansky on 3/27/16.
+//
+//
+
+#ifndef field_pool_h
+#define field_pool_h
+
+
+#endif /* field_pool_h */
