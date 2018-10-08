6472e1bea8b7383de1b26f2fc9fded77e3410d4a
Gleb Skaianski <gskaian@ip-1-70-93-109.dob1.bcpc.bloomberg.com>
2018-10-08 20:51:31 +0000

    initial commit

---
+++ octave/createInput.m
@@ -0,0 +1,33 @@
+## Copyright (C) 2018 Gleb Skaianski
+## 
+## This program is free software; you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or
+## (at your option) any later version.
+## 
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+## -*- texinfo -*- 
+## @deftypefn {} {@var{retval} =} createInput (@var{input1}, @var{input2})
+##
+## @seealso{}
+## @end deftypefn
+
+## Author: Gleb Skaianski <gskaian@ip-1-70-93-109>
+## Created: 2018-09-28
+
+createInput is the command-line function:
+
+				    function a = createInput (seed),
+  a = zeros (seed, 1);
+for i = 1:length (a),
+    a (i) = 2 ^ i;
+  endfor
+endfunction
+
