5ea75295ba945da147a0fe41d0a0277a12a2b6f3
Gleb Skaianski <gskaian@ip-1-70-93-109.dob1.bcpc.bloomberg.com>
2018-11-09 20:47:48 +0000

    async with and without await tests

--- py/zz.py
+++ py/zz.py
@@ -15,15 +15,23 @@ async def ablocking(name, delay, times):
         print(f"running blocking {name}. thread id {tid}")
         await asyncio.sleep(delay)
         times = times - 1
+async def ablocking_noawait(name, delay, times):
+    while(times):
+        tid = threading.current_thread().ident
+        print(f"running blocking {name}. thread id {tid}")
+        time.sleep(delay)
+        times = times - 1
 
 loop = asyncio.get_event_loop()
 loop.run_in_executor(None, blocking, "in-executor-3", 0.3, 10)
 loop.run_in_executor(None, blocking, "in-executor-2", 0.2, 10)
 loop.run_in_executor(None, blocking, "in-executor-1", 0.1, 10)
 
+loop.create_task(ablocking_noawait("on-event-loop-NoAwait", 0.5, 10))
 loop.create_task(ablocking("on-event-loop-1", 0.1, 5))
 loop.create_task(ablocking("on-event-loop-2", 0.15, 5))
 loop.create_task(ablocking("on-event-loop-3", 0.25, 5))
+
 
 loop.run_until_complete(asyncio.gather(*asyncio.Task.all_tasks(loop=loop)))
 
