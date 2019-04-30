18f2d0992b4bfec551a697ffd8fa09b99569eb22
gskayan <gskayan@gmail.com>
2019-04-29 21:14:07 -0400

    added list merge function

--- py/pypractice/pypractice/main.py
+++ py/pypractice/pypractice/main.py
@@ -8,10 +8,18 @@ def getprimelist( num : int ):
                 if 2 == len(r) and 1 == r[0] and i == r[1]:
                         print(i)
 
+def overlap_lists(x:list, y:list):
+        return [*set([*x, *y])]
+
 def main_impl():
         _n_ = inspect.currentframe().f_code.co_name
         print(f"Starting: {_n_}")
         getprimelist(int(sys.argv[1]))
+
+        a = range(0, 10, 3)
+        b = range(0, 20, 5)
+        
+        print(overlap_lists(a,b))
         print(f"Stopping: {_n_}")
 
 if __name__ =="__main__":
