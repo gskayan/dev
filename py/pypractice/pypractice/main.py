fdca1bdcb2ef46a8408f4d1fe2bbe4b7b002ad1f
Gleb Skaianski <gskaian@ip-1-70-93-113.dob1.bcpc.bloomberg.com>
2019-05-03 17:28:57 -0400

    refactoring

--- py/pypractice/pypractice/main.py
+++ py/pypractice/pypractice/main.py
@@ -2,9 +2,10 @@
 
 
 import argparse
-import inspect
-import sys
 import functools
+import inspect
+import mypracticefuncs as mypf
+import sys
 
 
 def print_func_name(f):
@@ -15,18 +16,10 @@ def print_func_name(f):
     return runit
 
 
-def _generate_prime_list(num: int):
-    primes = []
-    for i in range(1, num+1):
-        r = [d for d in range(1, num+1) if i % d == 0]
-        if 2 == len(r) and 1 == r[0] and i == r[1]:
-            primes.append(i)
-    return primes
-
 @print_func_name
 def getprimelist():
     num = int(input("Enter a number: "))
-    primes = _generate_prime_list(num)
+    primes = mypf._generate_prime_list(num)
     print(f"Primes up to {num} are [{primes}]")
 
 
@@ -36,24 +29,8 @@ def mergelists():
     a_str = input(f"Enter first list range <start>{d}<end>{d}[<step>]:")
     b_str = input(f"Enter second list range <start>{d}<end>{d}[<step>]:")
 
-    def str2rangeArgs(s: str, delim: str):
-        params = s
-        try:
-            intlist = [int(i) for i in s.split(delim) if len(i.strip()) > 0]
-        except ValueError as ve:
-            raise ValueError(f"Input format invalid: '{params}'")
-
-        if len(intlist) < 2:
-            raise Exception(f"Insufficient params for range: '{params}'")
-        if 2 == len(intlist):
-            return [*range(intlist[0], intlist[1])]
-        elif 3 >= len(intlist):
-            return [*range(intlist[0], intlist[1], intlist[2])]
-        else:
-            raise Exception(f"Range generation failed: '{params}'")
-
-    a = str2rangeArgs(a_str, d)
-    b = str2rangeArgs(b_str, d)
+    a = mypf.str2rangeArgs(a_str, d)
+    b = mypf.str2rangeArgs(b_str, d)
 
     res = [*set([*a, *b])]
     print(f"New list : {res}")
@@ -105,8 +82,6 @@ def main_impl():
                     help="Run funciton from the list")
     params = pp.parse_args()
     funcname = params.run
-
-    print(FUNCLIST)
 
     if funcname is not None:
         FUNCLIST[funcname]()
--- py/pypractice/pypractice/mypracticefuncs.py
+++ py/pypractice/pypractice/mypracticefuncs.py
@@ -7,3 +7,22 @@ def _generate_prime_list(num: int):
         if 2 == len(r) and 1 == r[0] and i == r[1]:
             primes.append(i)
     return primes
+
+
+
+def str2rangeArgs(s: str, delim: str):
+    params = s
+    try:
+        intlist = [int(i) for i in s.split(delim) if len(i.strip()) > 0]
+    except ValueError as ve:
+        raise ValueError(f"Input format invalid: '{params}'")
+
+    if len(intlist) < 2:
+        raise Exception(f"Insufficient params for range: '{params}'")
+
+    if 2 == len(intlist):
+        return [*range(intlist[0], intlist[1])]
+    elif 3 >= len(intlist):
+        return [*range(intlist[0], intlist[1], intlist[2])]
+    else:
+        raise Exception(f"Range generation failed: '{params}'")
