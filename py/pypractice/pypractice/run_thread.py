695571137713a583a676cafb8b543db8cdf83bb5
gskayan <gskayan@gmail.com>
2019-06-20 21:31:54 -0400

    tried simple rest clint

--- py/pypractice/pypractice/run_thread.py
+++ py/pypractice/pypractice/run_thread.py
@@ -1,6 +1,8 @@
 import time
 import threading
 
+from simple_rest_client.api import API
+
 
 keep_running = threading.Event()
 mut = threading.Lock()
@@ -17,8 +19,27 @@ def foo():
         #mut.release()
 
 
+def main_impl_2():
+    app = API (api_root_url="https://jsonplaceholder.typicode.com",
+               params={},
+               headers={},
+               timeout=2,
+               append_slash=False,
+               json_encode_body=True)
+    app.add_resource(resource_name="comments")
+    print(app.comments.actions)
 
-if __name__ == '__main__':
+    resp = app.comments.list(body=None, params={}, headers={})
+    print("{}\n{}\n{}\n{}\n".format(resp.url, resp.method, resp.headers, resp.body))
+
+    body=resp.body
+    for i,r in enumerate(body):
+        print(body[i]["name"])
+
+
+
+def main_impl_1():
+
     print("Starting Thread")
 
     tlist = []
@@ -35,3 +56,6 @@ if __name__ == '__main__':
 
     for t in tlist:
         t.join()
+
+if __name__ == '__main__':
+    main_impl_2()
